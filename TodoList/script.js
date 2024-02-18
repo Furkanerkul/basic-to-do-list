88eeaab801cda2c9c1e6d4602381c19817c4b667
Furkanerkul <155314035+Furkanerkul@users.noreply.github.com>
2024-02-18 23:37:44 +0300

    basic to do list

---
+++ TodoList/script.js
@@ -0,0 +1,12 @@
+function ekle(){
+    let metinKutusu = document.querySelector('input')
+    let liste = document.querySelector('ul')
+    let listeElementi = `<li>${metinKutusu.value} <button onclick="this.parentElement.remove()">Kaldır</button></li>`
+
+    if(metinKutusu.value != ""){
+    liste.insertAdjacentHTML('beforeend',listeElementi)
+    metinKutusu.value = ''
+    }else{
+        alert('Metin kutusu boş olmamalı')
+    }
+}
